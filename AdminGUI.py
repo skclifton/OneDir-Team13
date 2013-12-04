37d31f2f1950fa34891a01b45383a52771383f28
pm7dh <pm7dh@virginia.edu>
2013-12-03 21:56:03 -0500

    Deleted key display from AdminGUI file info option due to removal of encryption feature

--- AdminGUI.py
+++ AdminGUI.py
@@ -108,21 +108,21 @@ class LoggedOut(Frame):
             root.wm_title("View User Info")
             userinfo = urllib.urlopen(config.url + "/userinfo").read().split('\t')
             #print "{0:<20} {1:>20}".format("Username", "Password")
-            a = Label(root, text = "{0:<20} {1:>20} {2:>20}".format("Username", "Password", "Key"))
+            a = Label(root, text = "{0:<20} {1:>20}".format("Username", "Password"))
             a.pack()
             #print "-"*41
             b = Label(root, text = "-"*41)
             b.pack()
             i = 0
             j = 1
-            k = 2
-            while k < len(userinfo):
+            #k = 2
+            while j < len(userinfo):
                 #print "{0:20} {1:>20}".format(userinfo[i], userinfo[j])
-                c = Label(root, text = "{0:20} {1:>20} {2:>20}".format(userinfo[i], userinfo[j], userinfo[k]))
+                c = Label(root, text = "{0:20} {2:>20}".format(userinfo[i], userinfo[j]))
                 c.pack()
-                i += 3
-                j += 3
-                k += 3
+                i += 2
+                j += 2
+                #k += 3
 
         def view_sync_info():
             # print '---------- START SYNC HISTORY ----------'
